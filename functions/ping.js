14610c5ae43e9cc26d596bdb52cd7fa6549fdfcd
kiebu119 <kiebu119@gmail.com>
2026-09-15 17:04:00 -0700

    deleated 2 JS

--- functions/ping.js
+++
@@ -1,6 +0,0 @@
-// 動作確認用。DeepSeekを呼ばずに即座に返すだけの関数
-export function onRequest() {
-  return new Response(JSON.stringify({ ok: true, message: "functions are working" }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
